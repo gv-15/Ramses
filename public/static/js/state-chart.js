d8ccfe1c6ab34e561096526c3677e8cf2593825c
Maider Pozo Yubero <maiderpozo@gmail.com>
2022-02-04 09:56:58 +0100

    nearly achieved

--- public/static/js/state-chart.js
+++ public/static/js/state-chart.js
@@ -64,7 +64,14 @@ export default class StateChart {
         return JSON.stringify([{ "type": this.type, "sigma": this.sigma, "states": states }]);
         //return (JSON.stringify(states));
     }
-    
+    toDownload2() { //el objeto puede tener cosas que no se salvan, por eso creamos otros con lo que hay que salvar
+        let states = [];
+        this.states.forEach(st => states.push(st.toSave()));
+        console.log("states es ahora " + JSON.stringify([{ "type": this.type, "sigma": this.sigma, "states": states }]));
+        var dat = JSON.stringify([{ "type": this.type, "sigma": this.sigma, "states": states }]);
+       return this.OBJtoXML2(dat);
+        //return (JSON.stringify(states));
+    }
     toJSON() { //el objeto puede tener cosas que no se salvan, por eso creamos otros con lo que hay que salvar
         let states = [];
         this.states.forEach(st => states.push(st.toSave()));
@@ -197,5 +204,37 @@ export default class StateChart {
         let direct = (activeTransitions.map(tr => tr.to)); //estos estados pueden tener transiciones con epsilon
         return (direct.concat(this.siblings(direct)));
     }
+   
+     OBJtoXML2(obj){
+        var xml = '';
+        for (var prop in obj) 
+        {
+          xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
+          if (obj[prop] instanceof Array) 
+          {
+            for (var array in obj[prop]) 
+            {
+              xml += "<" + prop + ">";
+              xml += OBJtoXML(new Object(obj[prop][array]));
+              xml += "</" + prop + ">";
+            }
+          } 
+          else if (typeof obj[prop] == "object") 
+          {
+            xml += OBJtoXML(new Object(obj[prop]));
+    
+          } 
+          else 
+          {
+            xml += obj[prop];
+          }
+          
+          
+          xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
+        }
+        var xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
+        return xml;
+    }
+
 
 }
